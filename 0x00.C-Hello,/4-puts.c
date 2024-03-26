54e2d397adad423a184c8270123350f08bae95ef
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-27 00:29:03 +0300

    Changes

---
+++ 0x00.C-Hello,/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Use puts function to print the string
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate successful execution
+    return (0);
+}
+
