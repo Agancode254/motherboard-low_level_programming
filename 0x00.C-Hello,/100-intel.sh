469313e7ab174f9dfcd0bd7e95fe27cb6f41793c
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-27 00:43:51 +0300

    Changes

--- 0x00.C-Hello,/100-intel.sh
+++ 0x00.C-Hello,/100-intel.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
-gcc -S -masm=intel "$CFILE" -o "${CFILE%.c}.s"
+#include <stdio.h>
+
+int main(void)
+{
+    // Print the size of various types
+    printf("Size of a char: %ld byte(s)\n", sizeof(char));
+    printf("Size of an int: %ld byte(s)\n", sizeof(int));
+    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
 
