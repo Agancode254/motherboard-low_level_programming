260a49876497864f7d3bec9ee6fda36d644fa702
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-27 09:20:53 +0300

    Changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int r = 0; // Set r to 0
+
+    // Loop to print single-digit numbers from 0 to 9
+    for (int number = 0; number < 10; number++)
+    {
+        printf("%d\n", number); // Print the current number followed by a new line
+    }
+
+    return 0; // Indicate successful execution
+}
+
