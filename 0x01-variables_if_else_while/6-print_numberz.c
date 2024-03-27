d706f2626fc5fac9af4bea44467aef3e68bb8379
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-27 09:40:42 +0300

    Changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int main(void)
+{
+    // Save the number x
+    int x = '0';
+
+    // Use the for loop
+    for (x = '0'; x <= '9'; x++)
+        putchar(x);
+    putchar('\n');
+
+    return (0);
+}
+
+
